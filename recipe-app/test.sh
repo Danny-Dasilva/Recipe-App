9f2e2d7b739a6265f5f323a6dfd1dbfd420cfc78
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-07-26 19:27:05 -0400

    base

---
+++ recipe-app/test.sh
@@ -0,0 +1,4 @@
+sudo chown -R $(whoami) $ANDROID_HOME
+sudo apt-get install -y android-sdk-platform-tools
+sdkmanager "platform-tools" "platforms;android-29"
+androidsdk "platform-tools" "platforms;android-29"
